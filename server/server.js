de9ec1690bf2a7189e831a8f96eb8f09d04f3598
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-09-16 17:25:32 +0545

    fixed

--- server/server.js
+++ server/server.js
@@ -7,45 +7,14 @@ import LocationRoute from "./location.route.js";
 
 const app = express();
 
-const allowedOrigins = [
-  "https://insta-clone-five-bay.vercel.app",
-  "http://localhost:5173",
-];
+const corsOptions = {
+  origin: ["https://insta-clone-fswa.vercel.app", "http://localhost:3000"], // Add both production and localhost URLs
+  methods: "GET,POST,PUT,DELETE", // Allowed methods
+  credentials: true, // If using cookies or sessions
+};
 
-// CORS middleware
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
-// // CORS using the 'cors' package
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// Use CORS middleware
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
